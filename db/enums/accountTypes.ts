182fc66d9d1bbb18a7a1ab261a459db43f1b2022
Johan JANIN <johan.janin@gmail.com>
2026-01-08 15:54:34 +0100

    feat: roles migration; account module

---
+++ db/enums/accountTypes.ts
@@ -0,0 +1,6 @@
+export enum AccountTypes {
+  Student = 'student',
+  Company = 'company',
+  Admin = 'admin',
+  Managed = 'managed',
+}
---
+++ src/accounts/accounts.entity.ts
@@ -0,0 +1,24 @@
+import { AccountTypes } from 'db/enums/accountTypes';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Account {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar', {
+    unique: true,
+    length: 200,
+  })
+  email: string;
+
+  @Column('varchar', {
+    length: 200,
+  })
+  password: string;
+
+  @Column('enum', {
+    enum: AccountTypes,
+  })
+  type: AccountTypes;
+}
