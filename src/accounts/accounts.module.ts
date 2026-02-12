f67d98d1e33c7af62a6d1dbda4aeca4928efb6c0
Johan JANIN <johan.janin@gmail.com>
2026-02-12 15:30:29 +0100

    fix: account module import/exports

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -3,12 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
 import { Account } from './entities/account.entity';
+import { Administrator } from './entities/admin.entity';
+import { Company } from './entities/company.entity';
+import { Managed } from './entities/managed.entity';
 import { Student } from './entities/student.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account, Student])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Account,
+      Student,
+      Company,
+      Managed,
+      Administrator,
+    ]),
+  ],
   controllers: [AccountsController],
   providers: [AccountsService],
-  exports: [AccountsService, TypeOrmModule],
+  exports: [AccountsModule],
 })
 export class AccountsModule {}
