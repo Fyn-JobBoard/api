0f1cf4109fc213670145dc9376510011d33ea36f
Johan JANIN <johan.janin@gmail.com>
2026-02-12 11:19:05 +0100

    feat: fix import
    Signed-off-by: Johan JANIN <johan.janin@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountsModule } from './accounts/accounts.module';
+import { ActiveSearchesModule } from './active-searches/active-searches.module';
 import { ActivityDomainsModule } from './activity-domains/activity-domains.module';
 import { AppController } from './app.controller';
 import appDatasource from './app.datasource';
 import { AppService } from './app.service';
-import { JobsModule } from './jobs/jobs.module';
-import { StudentsModule } from './students/students.module';
-import { TagsModule } from './tags/tags.module';
-import { SkillsModule } from './skills/skills.module';
+import { ApplicationsModule } from './applications/applications.module';
 import { ExperiencesModule } from './experiences/experiences.module';
 import { FormationsModule } from './formations/formations.module';
-import { ApplicationsModule } from './applications/applications.module';
-import { ActiveSearchesModule } from './active-searches/active-searches.module';
+import { JobsModule } from './jobs/jobs.module';
+import { SkillsModule } from './skills/skills.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(appDatasource.options),
     AccountsModule,
-    StudentsModule,
     TagsModule,
     JobsModule,
     ActivityDomainsModule,
