d764d1cba9292056862cab19d8b71afb2bc8a65c
Johan JANIN <johan.janin@gmail.com>
2026-02-12 09:53:34 +0100

    fix: eslint issues on activity-domain

--- src/activity-domains/entities/activity-domain.entity.ts
+++ src/activity-domains/entities/activity-domain.entity.ts
@@ -2,24 +2,24 @@ import { Job } from 'src/jobs/entities/job.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('activity_domains')
 export class ActivityDomain {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-@PrimaryGeneratedColumn()
-    id: number;
-
-    @Column('varchar', {
+  @Column('varchar', {
     unique: true,
-    length: 60,})
-    name: string;
+    length: 60,
+  })
+  name: string;
 
-    @Column('varchar', {
+  @Column('varchar', {
     nullable: true,
-    length: 500,})
-    description?: string;
+    length: 500,
+  })
+  description?: string;
 
-    @OneToMany(
-    () => Job,
-    job => job.activity_domain
-  )
-    jobs: Job[];
-  
+  @OneToMany(() => Job, (job) => job.activity_domain, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  jobs: Job[];
 }
