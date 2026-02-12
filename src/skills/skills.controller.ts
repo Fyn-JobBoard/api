d219abe4adadb9ae487aa2379415c841e05704ad
Johan JANIN <johan.janin@gmail.com>
2026-02-12 12:24:52 +0100

    feat: skills controller getters (#20)

--- src/skills/skills.controller.ts
+++ src/skills/skills.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
+import { SkillTypes } from 'src/common/enums/skillsTypess';
 import { SkillsService } from './skills.service';
 
 @Controller('skills')
 export class SkillsController {
-  constructor(private readonly skillsService: SkillsService) {}
+  constructor(private readonly service: SkillsService) {}
+
+  @Get('/')
+  public async get(
+    @Query('name')
+    name?: string,
+    @Query('type')
+    type?: SkillTypes,
+  ) {
+    return this.service.get({ name, type });
+  }
+
+  @Get('/:id')
+  public async find(
+    @Param('id')
+    id: number,
+  ) {
+    const found = await this.service.find(id);
+    if (!found) throw new NotFoundException();
+
+    return found;
+  }
 }
