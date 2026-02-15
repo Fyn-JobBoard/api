77b1ba437f028781e8ea513db3c482f16697f2d6
Johan JANIN <johan.janin@gmail.com>
2026-02-15 18:52:00 +0100

    feat: setup tests for auth (#49)

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import appDatasource from 'src/app.datasource';
+import { Account } from 'src/accounts/entities/account.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: 'test',
+          signOptions: {
+            expiresIn: '1s',
+          },
+        }),
+        TypeOrmModule.forRoot(appDatasource.options),
+        TypeOrmModule.forFeature([Account]),
+      ],
       controllers: [AuthController],
       providers: [AuthService],
     }).compile();
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,6 +1,7 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AccountsModule } from 'src/accounts/accounts.module';
+import { Account } from 'src/accounts/entities/account.entity';
 import appDatasource from 'src/app.datasource';
 import { AuthService } from './auth.service';
 
@@ -9,7 +10,16 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forRoot(appDatasource.options), AccountsModule],
+      imports: [
+        JwtModule.register({
+          secret: 'test',
+          signOptions: {
+            expiresIn: '1s',
+          },
+        }),
+        TypeOrmModule.forRoot(appDatasource.options),
+        TypeOrmModule.forFeature([Account]),
+      ],
       providers: [AuthService],
     }).compile();
 
