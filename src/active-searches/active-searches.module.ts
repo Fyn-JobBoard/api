02b1172f65972144a102f73046c02f1c62216f67
Johan JANIN <johan.janin@gmail.com>
2026-02-15 12:57:49 +0100

    feat: active search controller (not finished) (#14)

--- src/active-searches/active-searches.module.ts
+++ src/active-searches/active-searches.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsService } from 'src/accounts/accounts.service';
+import { Account } from 'src/accounts/entities/account.entity';
 import { ActiveSearchesController } from './active-searches.controller';
 import { ActiveSearchesService } from './active-searches.service';
 import { ActiveSearch } from './entities/active-search.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ActiveSearch])],
+  imports: [TypeOrmModule.forFeature([ActiveSearch, Account])],
   controllers: [ActiveSearchesController],
-  providers: [ActiveSearchesService],
+  providers: [ActiveSearchesService, AccountsService],
   exports: [ActiveSearchesModule],
 })
 export class ActiveSearchesModule {}
