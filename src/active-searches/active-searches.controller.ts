6a086a3b39a1ed7a80f562b1d354591fc6f0ff09
Johan JANIN <johan.janin@gmail.com>
2026-02-15 12:57:49 +0100

    feat: active search controller (not finished) (#14)

--- src/active-searches/active-searches.controller.ts
+++ src/active-searches/active-searches.controller.ts
@@ -1,45 +1,106 @@
 import {
-  Body,
+  BadRequestException,
   Controller,
   Delete,
   Get,
+  HttpException,
+  NotFoundException,
+  NotImplementedException,
   Param,
-  Patch,
-  Post,
 } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiProperty } from '@nestjs/swagger';
+import { AccountsService } from 'src/accounts/accounts.service';
 import { ActiveSearchesService } from './active-searches.service';
-import { CreateActiveSearchDto } from './dto/create-active-search.dto';
-import { UpdateActiveSearchDto } from './dto/update-active-search.dto';
 
-@Controller('active-searches')
+@Controller('searches')
 export class ActiveSearchesController {
-  constructor(private readonly activeSearchesService: ActiveSearchesService) {}
+  constructor(
+    private readonly service: ActiveSearchesService,
+    private readonly accounts: AccountsService,
+  ) {}
 
-  @Post()
-  create(@Body() createActiveSearchDto: CreateActiveSearchDto) {
-    return this.activeSearchesService.create(createActiveSearchDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.activeSearchesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.activeSearchesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateActiveSearchDto: UpdateActiveSearchDto,
+  @Get('/:search_id')
+  @ApiProperty({
+    description: 'Get a single active search.',
+  })
+  @ApiParam({
+    name: 'search_id',
+    description: 'The id of the active search',
+    required: true,
+    type: 'string',
+  })
+  public async get(
+    @Param('search_id')
+    search_id: string,
   ) {
-    return this.activeSearchesService.update(+id, updateActiveSearchDto);
+    const id = parseInt(search_id);
+    if (isNaN(id))
+      throw new BadRequestException('search_id must be an integer.');
+
+    const search = await this.service.find(id);
+    if (!search) throw new NotFoundException();
+    return search;
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.activeSearchesService.remove(+id);
+  @Delete('/:search_id')
+  @ApiProperty({
+    description: 'Delete a single active search.',
+  })
+  @ApiParam({
+    name: 'search_id',
+    description: 'The id of the active search to delete',
+    required: true,
+  })
+  public async delete(
+    @Param('search_id')
+    search_id: string,
+  ) {
+    const id = parseInt(search_id);
+    if (isNaN(id))
+      throw new BadRequestException('search_id must be an integer.');
+
+    const deleted = await this.service.delete(id);
+    if (deleted instanceof HttpException) throw deleted;
+
+    return deleted;
+  }
+
+  // ---- For students ---- \\
+
+  @Get('/student/:student_id')
+  @ApiOperation({
+    description:
+      'Get all the active searches for the given student or the connected one.',
+  })
+  @ApiParam({
+    name: 'student_id',
+    required: false,
+    type: 'uuid',
+    description: 'The student id of which you want to get the active searches.',
+  })
+  public for(
+    @Param('student_id')
+    student_id: string,
+  ) {
+    throw new NotImplementedException();
+  }
+
+  @Delete('/student/:student_id')
+  @ApiOperation({
+    description:
+      'Delete all the active searches of the given student or the connected one.',
+  })
+  @ApiParam({
+    name: 'student_id',
+    required: false,
+    type: 'uuid',
+    description:
+      'The student id of which you want to delete the active searches.',
+  })
+  public clear(
+    @Param('student_id')
+    student_id: string,
+  ) {
+    throw new NotImplementedException();
   }
 }
