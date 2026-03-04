fecdb5f45affbffd91efc008aa8ca3841e2e2dd6
Johan JANIN <johan.janin@gmail.com>
2026-03-04 11:52:21 +0100

    feat: student controller

--- src/accounts/students.controller.ts
+++ src/accounts/students.controller.ts
@@ -1,8 +1,11 @@
 import {
+  Body,
   Controller,
   Get,
+  HttpException,
   NotFoundException,
   Param,
+  Put,
   Query,
   UseGuards,
   Version,
@@ -10,14 +13,21 @@ import {
 import {
   ApiBasicAuth,
   ApiBearerAuth,
+  ApiBody,
   ApiOkResponse,
+  ApiOperation,
   ApiParam,
   ApiQuery,
   ApiResponse,
 } from '@nestjs/swagger';
+import { AuthAccount } from 'src/auth/decorators/getters/account/account.decorator';
+import { IsA } from 'src/auth/guards/is-logged/decorators/is-a/is-a.decorator';
 import { IsLoggedGuard } from 'src/auth/guards/is-logged/is-logged.guard';
+import { AccountTypes } from 'src/common/enums/accountTypes';
 import { AccountsService } from './accounts.service';
 import { ListStudentsResponseDto } from './dto/students/list-students.response.dto';
+import { UpdateStudentDto } from './dto/students/update-student.dto';
+import { Account } from './entities/account.entity';
 import { Student } from './entities/student.entity';
 
 @Controller('accounts/students')
@@ -89,5 +99,63 @@ export class StudentsController {
     }
 
     return student;
+  }
+
+  @Put('/:id')
+  @UseGuards(IsLoggedGuard)
+  @Version('1')
+  @ApiOkResponse({
+    type: Student,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The id of the student you want to modify',
+  })
+  @ApiOkResponse({
+    type: Student,
+  })
+  @ApiBody({
+    type: UpdateStudentDto,
+  })
+  public async update(
+    @Param('id')
+    id: string,
+
+    @Body()
+    dto: UpdateStudentDto,
+  ) {
+    const updated = await this.accountsService.update(id, dto);
+    if (updated instanceof HttpException) {
+      throw updated;
+    }
+
+    return updated as Student;
+  }
+
+  @Put('/')
+  @UseGuards(IsLoggedGuard)
+  @IsA([AccountTypes.Student])
+  @Version('1')
+  @ApiOkResponse({
+    type: Student,
+  })
+  @ApiOkResponse({
+    type: Student,
+  })
+  @ApiBody({
+    type: UpdateStudentDto,
+  })
+  @ApiOperation({
+    description:
+      'Update your own account. You must be logged as a student account.',
+  })
+  public async update_me(
+    @Body()
+    dto: UpdateStudentDto,
+
+    @AuthAccount()
+    auth: Account,
+  ) {
+    return this.update(auth.id, dto);
   }
 }
