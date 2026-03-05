dd694217582d3b5c79ea0b201712cf584ea11848
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-03-05 12:03:14 +0100

    fix IsLoggedGuard inside the ActivityDomainsModule.

--- src/activity-domains/activity-domains.module.ts
+++ src/activity-domains/activity-domains.module.ts
@@ -3,11 +3,12 @@ import { ActivityDomainsService } from './activity-domains.service';
 import { ActivityDomainsController } from './activity-domains.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityDomain } from './entities/activity-domain.entity';
-
+import { AccountsModule } from 'src/accounts/accounts.module';
+import { IsLoggedGuard } from 'src/auth/guards/is-logged/is-logged.guard';
 @Module({
-  imports: [TypeOrmModule.forFeature([ActivityDomain])],
+  imports: [TypeOrmModule.forFeature([ActivityDomain]), AccountsModule],
   controllers: [ActivityDomainsController],
-  providers: [ActivityDomainsService],
+  providers: [ActivityDomainsService, IsLoggedGuard],
   exports: [ActivityDomainsService],
 })
 export class ActivityDomainsModule {}
