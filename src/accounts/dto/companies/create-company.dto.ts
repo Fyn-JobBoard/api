0baae3442af7c3926f02ecc050dd409a2e636746
Johan JANIN <johan.janin@gmail.com>
2026-03-06 16:53:48 +0100

    feat: company dto swagger typing

--- src/accounts/dto/companies/create-company.dto.ts
+++ src/accounts/dto/companies/create-company.dto.ts
@@ -1,22 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsDefined, IsString, IsUrl, MaxLength } from 'class-validator';
 
 export class CreateCompanyDto {
   @IsDefined()
   @MaxLength(250)
+  @ApiProperty({
+    type: 'string',
+    required: true,
+  })
   name: string;
 
   @IsString()
+  @ApiProperty({
+    type: 'string',
+    format: 'html',
+    required: false,
+  })
   bio?: string;
 
   @IsDefined()
   @IsDate()
+  @ApiProperty({
+    type: 'string',
+    format: 'date',
+  })
   creation_date: Date;
 
   @IsString()
   @IsUrl()
+  @ApiProperty({
+    type: 'string',
+    required: false,
+    format: 'url',
+  })
   scrapped_from?: string;
 
   @IsString()
   @IsUrl()
+  @ApiProperty({
+    type: 'string',
+    format: 'url',
+    required: false,
+  })
   website_url?: string;
 }
