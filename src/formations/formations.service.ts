7888d490c26e13e3da1d0653e044c8b19d93ae42
Johan JANIN <johan.janin@gmail.com>
2026-03-22 16:54:49 +0100

    formations service (#38)

--- src/formations/formations.service.ts
+++ src/formations/formations.service.ts
@@ -1,26 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from 'src/accounts/entities/student.entity';
+import { FindOptionsWhere, Repository } from 'typeorm';
 import { CreateFormationDto } from './dto/create-formation.dto';
 import { UpdateFormationDto } from './dto/update-formation.dto';
+import { Formation } from './entities/formation.entity';
 
 @Injectable()
 export class FormationsService {
-  create(createFormationDto: CreateFormationDto) {
-    return 'This action adds a new formation';
+  constructor(
+    @InjectRepository(Formation)
+    private readonly formations: Repository<Formation>,
+  ) {}
+
+  async create(createFormationDto: CreateFormationDto, student: Student) {
+    const { identifiers } = await this.formations.insert({
+      ...createFormationDto,
+      student,
+    });
+
+    return this.formations.findOneByOrFail({
+      id: identifiers[0].id as number,
+    });
   }
 
-  findAll() {
-    return `This action returns all formations`;
+  public async list(
+    page: number = 1,
+    per_page?: number,
+    where?: FindOptionsWhere<Formation>,
+  ) {
+    if (per_page === undefined) {
+      return {
+        page: 1,
+        pages: 1,
+        list: await this.formations.find({
+          where,
+        }),
+      };
+    }
+    const amount = await this.formations.count();
+    return {
+      page,
+      pages: Math.ceil(amount / per_page),
+      list: await this.formations.find({
+        skip: per_page * (page - 1),
+        take: per_page,
+        where,
+      }),
+    };
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} formation`;
+    return this.formations.findOneBy({ id });
   }
 
-  update(id: number, updateFormationDto: UpdateFormationDto) {
-    return `This action updates a #${id} formation`;
+  async update(id: number, updateFormationDto: UpdateFormationDto) {
+    if (!(await this.formations.existsBy({ id }))) {
+      return new NotFoundException();
+    }
+
+    await this.formations.update({ id }, updateFormationDto);
+    return this.formations.findOneByOrFail({ id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} formation`;
+  async remove(id: number) {
+    const formation = await this.findOne(id);
+    if (!formation) {
+      return new NotFoundException();
+    }
+
+    await this.formations.delete(formation);
+
+    return formation;
+  }
+
+  /**
+   * Remove all formations of a student
+   */
+  async clear(student: Student) {
+    const cleared = await this.list(undefined, undefined, {
+      student,
+    }).then((res) => res.list);
+    await this.formations.delete({
+      student,
+    });
+
+    return cleared;
   }
 }
