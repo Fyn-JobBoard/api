cb85f416c5b7a9f5ddc4ba3a02f2fb9ece20abad
Johan JANIN <johan.janin@gmail.com>
2026-03-24 14:35:27 +0100

    feat: get routes of formations

--- src/formations/formations.controller.ts
+++ src/formations/formations.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   NotFoundException,
   Param,
   Post,
+  Query,
   UnauthorizedException,
   UseGuards,
   Version,
@@ -14,6 +16,7 @@ import {
   ApiOkResponse,
   ApiOperation,
   ApiParam,
+  ApiQuery,
   ApiResponse,
 } from '@nestjs/swagger';
 import { AccountsService } from 'src/accounts/accounts.service';
@@ -26,6 +29,7 @@ import { IsLoggedGuard } from 'src/auth/guards/is-logged/is-logged.guard';
 import { AccountTypes } from 'src/common/enums/accountTypes';
 import { Permissions } from 'src/common/enums/permissions';
 import { CreateFormationDto } from './dto/create-formation.dto';
+import { ListFormationsResponseDto } from './dto/list-formation-response.dto';
 import { Formation } from './entities/formation.entity';
 import { FormationsService } from './formations.service';
 
@@ -82,23 +86,100 @@ export class FormationsController {
     return this.formations.create(formationDto, student);
   }
 
-  @Post('/')
+  @Get('/')
   @UseGuards(IsLoggedGuard)
   @IsA([AccountTypes.Student])
   @Version('1')
   @ApiOperation({
-    description:
-      'Create a new formation and asign it to yourself (only available for students)',
+    description: 'Get all your formations (only available for students)',
   })
   @ApiOkResponse({
-    type: Formation,
+    type: ListFormationsResponseDto,
   })
-  async create(
-    @Body()
-    formationDto: CreateFormationDto,
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: 'The page shift',
+    type: 'integer',
+    minimum: 1,
+  })
+  @ApiQuery({
+    name: 'per_page',
+    required: false,
+    description: 'The amount of items you want in a page',
+    type: 'integer',
+    minimum: 1,
+  })
+  async list(
     @AuthAccount()
     auth: Account,
+
+    @Query('page', {
+      transform: (v?: string) => parseInt(v ?? ''),
+    })
+    page: number,
+    @Query('per_page', {
+      transform: (v?: string) => parseInt(v ?? ''),
+    })
+    per_page: number,
   ) {
-    return this.createFor(formationDto, auth.id, auth);
+    return this.listFor(auth.id, page, per_page);
+  }
+
+  @Get('/:student_id')
+  @UseGuards(IsLoggedGuard)
+  @Version('1')
+  @IsManagedAnd({
+    permissions: (perms) => perms.hasAll(Permissions.VIEW_FORMATIONS),
+  })
+  @ApiParam({
+    name: 'student_id',
+    description: 'The id of the student you want to view the formations.',
+  })
+  @ApiOperation({
+    description: 'View all formations of a student',
+  })
+  @ApiOkResponse({
+    type: ListFormationsResponseDto,
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: 'The page shift',
+    type: 'integer',
+    minimum: 1,
+  })
+  @ApiQuery({
+    name: 'per_page',
+    required: false,
+    description: 'The amount of items you want in a page',
+    type: 'integer',
+    minimum: 1,
+  })
+  async listFor(
+    @Param('student_id')
+    student_id: string,
+
+    @Query('page', {
+      transform: (v?: string) => parseInt(v ?? ''),
+    })
+    page: number,
+    @Query('per_page', {
+      transform: (v?: string) => parseInt(v ?? ''),
+    })
+    per_page: number,
+  ) {
+    const student = await this.accounts.findModel(student_id, Student);
+    if (!student) {
+      throw new NotFoundException(`Student #${student_id} does not exists.`);
+    }
+
+    return this.formations.list(
+      isNaN(page) ? undefined : Math.max(1, page),
+      isNaN(per_page) ? 20 : Math.max(1, per_page),
+      {
+        student,
+      },
+    );
   }
 }
