f45e0efb8a586f2b8be7c691b4900a307c023c1d
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-03-25 21:22:03 +0100

    fix tags.controller.spec.ts for CI tests

--- src/tags/tags.controller.spec.ts
+++ src/tags/tags.controller.spec.ts
@@ -3,6 +3,7 @@ import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Tag } from './entities/tag.entity';
+import { IsLoggedGuard } from 'src/auth/guards/is-logged/is-logged.guard';
 
 describe('TagsController', () => {
   let controller: TagsController;
@@ -17,7 +18,10 @@ describe('TagsController', () => {
           useValue: {},
         },
       ],
-    }).compile();
+    })
+      .overrideGuard(IsLoggedGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     controller = module.get<TagsController>(TagsController);
   });
