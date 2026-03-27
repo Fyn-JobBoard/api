0e25a43ac411e1f9e0970039b6ba0d06f370dea9
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-03-27 20:49:47 +0100

    patch tags.module.ts

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -3,10 +3,11 @@ import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from './entities/tag.entity';
+import { AccountsModule } from 'src/accounts/accounts.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag])],
+  imports: [TypeOrmModule.forFeature([Tag]), AccountsModule],
   controllers: [TagsController],
   providers: [TagsService],
-  exports: [TagsModule],
+  exports: [TagsService],
 })
 export class TagsModule {}
