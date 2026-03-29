6f915e534f877011f565fb8a4ce9b8bd3ed44616
Johan JANIN <johan.janin@gmail.com>
2026-03-29 17:55:05 +0200

    db ssl usage option

--- src/app.datasource.ts
+++ src/app.datasource.ts
@@ -9,6 +9,9 @@ export default new DataSource({
   port: +(process.env.DB_PORT ?? 5433),
   username: process.env.DB_USERNAME ?? 'postgres',
   password: process.env.DB_PASSWORD ?? '',
+  ssl:
+    typeof process.env.DB_SSL === 'string' &&
+    ['yes', '1', 'with', 'true'].includes(process.env.DB_SSL),
   entities: [join(__dirname, '/**/*.entity.{js,ts}')],
   migrations: [join(__dirname, '../db/migrations/*.{ts,js}')],
 });
