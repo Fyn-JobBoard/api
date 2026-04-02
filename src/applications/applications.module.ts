7b2c4c458ec9d4ab5a19f56b69b95c0b09c4793b
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-04-02 21:35:42 +0200

    fix(applications): register Job and Student repositories in module

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -4,9 +4,14 @@ import { ApplicationsController } from './applications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Application } from './entities/application.entity';
 import { AccountsModule } from 'src/accounts/accounts.module';
+import { Job } from 'src/jobs/entities/job.entity';
+import { Student } from 'src/accounts/entities/student.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Application]), AccountsModule],
+  imports: [
+    TypeOrmModule.forFeature([Application, Job, Student]),
+    AccountsModule,
+  ],
   controllers: [ApplicationsController],
   providers: [ApplicationsService],
   exports: [ApplicationsService],
