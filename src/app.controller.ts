d4ae7bd8dede1d937ffa4fd4adb863753bd969a5
Johan JANIN <johan.janin@gmail.com>
2026-04-15 14:34:23 +0200

    feat: up route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Version } from '@nestjs/common';
+import { Controller, Get, Query, Req, Version } from '@nestjs/common';
+import { type Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   @Version('1')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(
+    @Req()
+    request: Request,
+    @Query('only')
+    only?: string,
+  ) {
+    const ping = this.appService.getPing(request);
+    if (only && only in ping) {
+      return ping[only] as unknown;
+    }
+    return ping;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { type Request } from 'express';
+import * as project from '../package.json';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPing(request: Request) {
+    return {
+      uptime: Math.floor(process.uptime() * 1e3),
+      version: project.version,
+      client_ip: request.ip,
+      api_version: 1,
+    };
   }
 }
