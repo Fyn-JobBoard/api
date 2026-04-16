100c4f8cb8f07821207d37047b95b79190fff8c7
Johan JANIN <johan.janin@gmail.com>
2026-04-16 10:55:01 +0200

    feat: tests & better active search module

--- src/active-searches/active-searches.controller.spec.ts
+++ src/active-searches/active-searches.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsModule } from 'src/accounts/accounts.module';
+import appDatasource from 'src/app.datasource';
 import { ActiveSearchesController } from './active-searches.controller';
 import { ActiveSearchesService } from './active-searches.service';
+import { ActiveSearch } from './entities/active-search.entity';
 
 describe('ActiveSearchesController', () => {
   let controller: ActiveSearchesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(appDatasource.options),
+        TypeOrmModule.forFeature([ActiveSearch]),
+        AccountsModule,
+      ],
       controllers: [ActiveSearchesController],
       providers: [ActiveSearchesService],
     }).compile();
