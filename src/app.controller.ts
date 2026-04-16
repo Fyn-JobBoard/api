23f8b167a7629dfad484015a2c43ce579b288f44
Johan JANIN <johan.janin@gmail.com>
2026-04-16 11:52:41 +0200

    feat: better swagger doc for up route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query, Req, Version } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { type Request } from 'express';
 import { AppService } from './app.service';
 
@@ -8,6 +9,42 @@ export class AppController {
 
   @Get('/')
   @Version('1')
+  @ApiOperation({
+    description: 'Ping the application and get some informations about it',
+  })
+  @ApiQuery({
+    name: 'only',
+    required: false,
+    description: 'Provide the key you only want to return',
+  })
+  @ApiOkResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        uptime: {
+          type: 'integer',
+          minimum: 0,
+          description: 'The uptime of the API (in ms)',
+        },
+        version: {
+          type: 'string',
+          format: 'version',
+          description: 'The version of the API project',
+        },
+        client_ip: {
+          type: 'string',
+          format: 'ip',
+          description: 'Your IP',
+        },
+        api_version: {
+          type: 'integer',
+          minimum: 1,
+          description: 'The latest api version',
+        },
+      },
+      required: ['uptime', 'version', 'api_version'],
+    },
+  })
   getPing(
     @Req()
     request: Request,
