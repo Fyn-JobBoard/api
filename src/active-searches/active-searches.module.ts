3bd6ab18212097f567ecea71e69f7283765aa3a2
Johan JANIN <johan.janin@gmail.com>
2026-04-16 10:55:01 +0200

    feat: tests & better active search module

--- src/active-searches/active-searches.module.ts
+++ src/active-searches/active-searches.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AccountsService } from 'src/accounts/accounts.service';
-import { Account } from 'src/accounts/entities/account.entity';
+import { AccountsModule } from 'src/accounts/accounts.module';
 import { ActiveSearchesController } from './active-searches.controller';
 import { ActiveSearchesService } from './active-searches.service';
 import { ActiveSearch } from './entities/active-search.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ActiveSearch, Account])],
+  imports: [TypeOrmModule.forFeature([ActiveSearch]), AccountsModule],
   controllers: [ActiveSearchesController],
-  providers: [ActiveSearchesService, AccountsService],
-  exports: [ActiveSearchesModule],
+  providers: [ActiveSearchesService],
+  exports: [ActiveSearchesService],
 })
 export class ActiveSearchesModule {}
