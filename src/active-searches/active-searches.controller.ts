9ca703c3fe0c5d80b0d826b69c57f36e7e6f62ad
Johan JANIN <johan.janin@gmail.com>
2026-04-16 10:50:17 +0200

    feat: active search controller & service (#14 & #34)

--- src/active-searches/active-searches.controller.ts
+++ src/active-searches/active-searches.controller.ts
@@ -1,29 +1,36 @@
 import {
+  Body,
   Controller,
   Get,
+  HttpException,
   NotFoundException,
-  NotImplementedException,
   Param,
+  Post,
   Put,
+  UnauthorizedException,
   UseGuards,
   Version,
 } from '@nestjs/common';
 import {
   ApiBasicAuth,
   ApiBearerAuth,
+  ApiBody,
   ApiOkResponse,
   ApiOperation,
   ApiParam,
   ApiResponse,
 } from '@nestjs/swagger';
 import { AccountsService } from 'src/accounts/accounts.service';
+import { Student } from 'src/accounts/entities/student.entity';
 import { Auth } from 'src/auth/class/auth.class';
 import { AuthAccount } from 'src/auth/decorators/getters/account/account.decorator';
+import { IsA } from 'src/auth/guards/is-logged/decorators/is-a/is-a.decorator';
 import { IsManagedAnd } from 'src/auth/guards/is-logged/decorators/is-managed-and/is-managed-and.decorator';
 import { IsLoggedGuard } from 'src/auth/guards/is-logged/is-logged.guard';
 import { AccountTypes } from 'src/common/enums/accountTypes';
 import { Permissions } from 'src/common/enums/permissions';
 import { ActiveSearchesService } from './active-searches.service';
+import { SearchPredicates } from './dto/search-predicates.dto';
 import { ActiveSearch } from './entities/active-search.entity';
 
 @Controller('searches')
@@ -66,10 +73,10 @@ export class ActiveSearchesController {
   ) {
     const search = await this.searchesService.find(id);
     if (
-      !search ||
       !(
-        [AccountTypes.Admin, AccountTypes.Managed].includes(auth.type) ||
-        search.student.id === auth.id
+        search &&
+        ([AccountTypes.Admin, AccountTypes.Managed].includes(auth.type) ||
+          search.student.id === auth.id)
       )
     ) {
       throw new NotFoundException();
@@ -83,6 +90,19 @@ export class ActiveSearchesController {
   @IsManagedAnd({
     permissions: (perms) => perms.hasAll(Permissions.MANAGE_ACTIVE_SEARCHES),
   })
+  @ApiBody({
+    type: () => SearchPredicates,
+  })
+  @ApiOkResponse({
+    type: () => ActiveSearch,
+  })
+  @ApiOperation({
+    description: 'Update an existing search predicate',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'integer',
+  })
   public async edit(
     @Param('id', {
       transform: (value: string) => parseInt(value),
@@ -91,18 +111,162 @@ export class ActiveSearchesController {
 
     @AuthAccount()
     auth: Auth,
+
+    @Body()
+    criterias: SearchPredicates,
   ) {
     const search = await this.searchesService.find(id);
     if (
-      !search ||
       !(
-        [AccountTypes.Admin, AccountTypes.Managed].includes(auth.type) ||
-        search.student.id === auth.id
+        search &&
+        ([AccountTypes.Admin, AccountTypes.Managed].includes(auth.type) ||
+          search.student.id === auth.id)
       )
     ) {
       throw new NotFoundException();
     }
 
-    throw new NotImplementedException();
+    const result = await this.searchesService.update(search, criterias);
+    if (result instanceof HttpException) {
+      throw result;
+    }
+
+    return result;
+  }
+
+  @Get('/of/:student_id')
+  @IsManagedAnd({
+    permissions: (perms) => perms.hasAll(Permissions.VIEW_ACTIVE_SEARCHES),
+  })
+  @Version('1')
+  @ApiOkResponse({
+    type: () => ActiveSearch,
+    isArray: true,
+  })
+  @ApiParam({
+    name: 'student_id',
+    type: 'string',
+    format: 'uuid',
+    required: true,
+  })
+  @ApiOperation({
+    description:
+      'Get all the active search of a student. As a student, you can oonly access to yours',
+  })
+  public async allOfStudent(
+    @Param('student_id')
+    student_id: string,
+
+    @AuthAccount()
+    auth: Auth,
+  ) {
+    if (
+      !(
+        [AccountTypes.Admin, AccountTypes.Managed].includes(auth.type) ||
+        student_id === auth.id
+      )
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    const student = await this.accounts.findModel(student_id, Student);
+    if (!student) {
+      throw new NotFoundException();
+    }
+
+    return this.searchesService.allOf(student);
+  }
+
+  @Post('/of/:student_id')
+  @Version('1')
+  @IsManagedAnd({
+    permissions: (perms) => perms.hasAll(Permissions.MANAGE_ACTIVE_SEARCHES),
+  })
+  @ApiBody({
+    type: () => SearchPredicates,
+  })
+  @ApiOperation({
+    description: 'Register a new active search for the given student',
+  })
+  @ApiOkResponse({
+    type: () => ActiveSearch,
+  })
+  @ApiParam({
+    name: 'student_id',
+    type: 'string',
+    format: 'uuid',
+    required: true,
+  })
+  public async create(
+    @Param('student_id')
+    student_id: string,
+
+    @Body()
+    criterias: SearchPredicates,
+
+    @AuthAccount()
+    auth: Auth,
+  ) {
+    if (
+      !(
+        [AccountTypes.Admin, AccountTypes.Managed].includes(auth.type) ||
+        student_id === auth.id
+      )
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    const student = await this.accounts.findModel(student_id, Student);
+    if (!student) {
+      throw new NotFoundException();
+    }
+
+    const result = await this.searchesService.create(student, criterias);
+    if (result instanceof HttpException) {
+      throw result;
+    }
+
+    return result;
+  }
+
+  // For students only \\
+
+  @Get('/me')
+  @Version('1')
+  @ApiOperation({
+    description: 'As a student, get all my active search',
+  })
+  @IsA([AccountTypes.Student])
+  @ApiOkResponse({
+    type: () => ActiveSearch,
+    isArray: true,
+  })
+  public async allOfMe(
+    @AuthAccount()
+    auth: Auth,
+  ) {
+    return this.allOfStudent(auth.id, auth);
+  }
+
+  @Post('/me')
+  @Version('1')
+  @ApiBody({
+    type: () => SearchPredicates,
+  })
+  @IsA([AccountTypes.Student])
+  @ApiOperation({
+    description: 'As a student, register a new active search for me',
+  })
+  @ApiOkResponse({
+    type: () => ActiveSearch,
+  })
+  public async createForMe(
+    @Body()
+    criterias: SearchPredicates,
+
+    @AuthAccount()
+    auth: Auth,
+  ) {
+    return this.create(auth.id, criterias, auth);
   }
 }
--- src/active-searches/active-searches.service.ts
+++ src/active-searches/active-searches.service.ts
@@ -1,18 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AccountsService } from 'src/accounts/accounts.service';
+import { Student } from 'src/accounts/entities/student.entity';
 import { Repository } from 'typeorm';
+import { SearchPredicates } from './dto/search-predicates.dto';
 import { ActiveSearch } from './entities/active-search.entity';
 
 @Injectable()
 export class ActiveSearchesService {
+  protected static readonly V1_MAX_ACTIVE_SEARCH_PER_STUDENTS = 10;
+
   constructor(
     @InjectRepository(ActiveSearch)
     private readonly searches: Repository<ActiveSearch>,
-    private readonly accounts: AccountsService,
   ) {}
 
-  public async find(id: number) {
-    return this.searches.findOneBy({ id });
+  public async find(id: number, without_student_info = false) {
+    const relations: string[] = [];
+    if (!without_student_info) {
+      relations.push('student');
+    }
+
+    return this.searches.findOne({ where: { id }, relations });
+  }
+
+  public async create(student: Student, criterias: SearchPredicates) {
+    if (
+      (await this.searches.countBy({ student })) >=
+      ActiveSearchesService.V1_MAX_ACTIVE_SEARCH_PER_STUDENTS
+    ) {
+      return new NotAcceptableException(
+        `The student #${student.id} cannot have more than ${ActiveSearchesService.V1_MAX_ACTIVE_SEARCH_PER_STUDENTS} active searches.`,
+      );
+    }
+
+    return this.searches.save(this.searches.create({ student, criterias }));
+  }
+
+  public async update(
+    search: number | ActiveSearch,
+    criterias: SearchPredicates,
+  ): Promise<ActiveSearch | HttpException> {
+    const active_search =
+      typeof search === 'number' ? await this.find(search) : search;
+
+    if (!active_search) {
+      return new NotFoundException();
+    }
+
+    active_search.criterias = criterias;
+    return this.searches.save(active_search);
+  }
+
+  public async allOf(
+    student: Student,
+    without_student_info = false,
+  ): Promise<ActiveSearch[]> {
+    const relations: string[] = [];
+    if (!without_student_info) {
+      relations.push('student');
+    }
+
+    return this.searches.find({ where: { student }, relations });
   }
 }
